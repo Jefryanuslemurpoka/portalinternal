ad145c8e897dbf2d056c13d07f588815d2e6bd60
jefryanuslemurpoka <jefrylemur@gmail.com>
2025-12-12 08:54:51 +0700

    update portal internal v.1.3.2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust proxies (untuk production nanti)
+        $middleware->trustProxies(at: '*');
+        
         // Register custom middleware aliases
         $middleware->alias([
             'superadmin' => \App\Http\Middleware\SuperAdminMiddleware::class,
